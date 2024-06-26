9bf3053ff2ed0efdd7f8339b650a3652a4f1f5ac
Bloduc Spauter <3230695439@qq.com>
2024-06-27 00:17:44 +0800

    提交

--- fe-blog-system-category/src/main/java/com/bloducspauter/mapper/TagMapper.java
+++ fe-blog-system-category/src/main/java/com/bloducspauter/mapper/TagMapper.java
@@ -13,7 +13,7 @@ public interface TagMapper extends BaseMapper<Tag> {
     /**
      * 根据博客id查询全部对应的标签
      */
-    @Select("select name,tag_id from db_mybatis.tag where tag_id = any(select tag_id from db_mybatis.tag_relation " +
+    @Select("select name,tag_id from tag where tag_id = any(select tag_id from tag_relation " +
             "where blog_id = #{blogId} and deleted = 0) ")
     List<Tag> selectTagsByBlog(int blogId);
 }
