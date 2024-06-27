ad6bb7de2a78a2555cdca94ec3edaaf88463453f
Bloduc Spauter <3230695439@qq.com>
2024-06-27 14:40:52 +0800

    移除前端index

--- fe-blog-system-index/src/main/resources/static/js/common.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    /* 引入layui */
-    layui.use(['element','laydate','upload','form'],function(){
-        let element = layui.element;
-        let form = layui.form;
-    })
-
-})
